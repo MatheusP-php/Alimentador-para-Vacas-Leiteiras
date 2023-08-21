f37f21b34d0ac4f58724313e49d32dbab8228e85
Matheus Primo <83077665+MatheusP-php@users.noreply.github.com>
2023-08-21 11:32:46 -0300

    Update README.ino

--- README.ino
+++ README.ino
@@ -1,3 +1,6 @@
 Alimentador de vacas para reposição de nutrientes, projeto de TCC.
 
+Linguagens usadas C# .Net, SQL, Arduino C.
  
+ ![2023-08-21-11-21-55](https://github.com/MatheusP-php/Alimentador-para-Vacas-Leiteiras/assets/83077665/a3d5a9a5-0d24-4d11-b192-1709bc8ca63b)
+
